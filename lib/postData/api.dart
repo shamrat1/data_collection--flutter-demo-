5ea1bea466b89a625ac0998d0bc02189f9b9966a
samuchakraborty <samuckb@gmail.com>
2021-03-18 23:16:11 +0600

    send api created

--- lib/postData/api.dart
+++ lib/postData/api.dart
@@ -1,52 +1,57 @@
 import 'dart:convert';
-import 'dart:io';
+
 import 'package:awesome_dialog/awesome_dialog.dart';
-import 'package:dio/dio.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
+import 'package:flutter/cupertino.dart';
 
 class NetWork {
-  var baseUrl = "http://139.59.112.145/api";
+  
   Map<String, String> headers = {
-   "Content-type": "application/json",
+    "Content-type": "application/json",
     'Charset': 'utf-8'
   };
   sendHospitalStore({
     @required context,
     @required String name,
-    @required String name_bn,
-    @required String city_id,
-    @required String division_id,
-    @required String address_line_1,
-    String address_line_2,
-    List services,
-    List surgeries,
-    List test_facilities,
+    @required String nameBangla,
+    @required String cityId,
+    @required String divisionId,
+    @required String addressLine1,
+    String addressLine2,
+     services,
+     surgeries,
+     testFacilities,
     @required image,
-    @required String location_lat,
-    @required String location_lng,
-    @required String branch_name,
-    @required String reception_phone,
+    @required String locationLat,
+    @required String locationLng,
+    @required String branchName,
+    @required String receptionPhone,
   }) async {
-    var fullUrl = baseUrl + "/registration/hospital/store";
+    //   var fullUrl = baseUrl + "/registration/hospital/store";
+
+    var fullUrl = Uri(
+        scheme: "http",
+        host: "139.59.112.145",
+        path: "/api/registration/hospital/store");
 
     var data = {
       'name': name,
-      'name_bn': name_bn,
-      'city_id': city_id,
-      'division_id': division_id,
-      'address_line_1': address_line_1,
-      'address_line_2': address_line_2,
-     
-      'location_lat': location_lat,
-      'location_lng': location_lng,
-      'branch_name': branch_name,
-      'reception_phone': reception_phone,
-       'image': image,
+      'nameBangla': nameBangla,
+      'cityId': cityId,
+      'division_id': divisionId,
+      'address_line_1': addressLine1,
+      'address_line_2': addressLine2,
+      'Services': services,
+      'Surgeries': surgeries,
+      'test_facilities': testFacilities,
+      'location_lat': locationLat,
+      'location_lng': locationLng,
+      'branch_name': branchName,
+      'receptionPhone': receptionPhone,
+      'image': image,
     };
 
-   // FormData formData = new FormData.fromMap(data);
-
+    // FormData formData = new FormData.fromMap(data);
 
     final response =
         await http.post(fullUrl, body: json.encode(data), headers: headers);
@@ -82,4 +87,172 @@ class NetWork {
       )..show();
     }
   }
+
+
+
+
+
+sendDoctorStore({
+    @required context,
+    @required String name,
+    @required String nameBangla,
+    @required String cityId,
+    @required String divisionId,
+    @required String addressLine1,
+    String addressLine2,
+     services,
+     surgeries,
+     testFacilities,
+    @required image,
+    @required String locationLat,
+    @required String locationLng,
+    @required String branchName,
+    @required String receptionPhone,
+  }) async {
+   
+    var fullUrl = Uri(
+        scheme: "http",
+        host: "139.59.112.145",
+        path: "/api/registration/doctor/store");
+
+    var data = {
+      'name': name,
+      'nameBangla': nameBangla,
+      'cityId': cityId,
+      'division_id': divisionId,
+      'address_line_1': addressLine1,
+      'address_line_2': addressLine2,
+      'Services': services,
+      'Surgeries': surgeries,
+      'test_facilities': testFacilities,
+      'location_lat': locationLat,
+      'location_lng': locationLng,
+      'branch_name': branchName,
+      'receptionPhone': receptionPhone,
+      'image': image,
+    };
+
+    
+
+    final response =
+        await http.post(fullUrl, body: json.encode(data), headers: headers);
+
+    String showMessage;
+
+    var body = json.decode(response.body);
+    if (response.statusCode == 200) {
+      showMessage = body['msg'];
+      print(data);
+      print(body);
+      AwesomeDialog(
+        context: context,
+        dialogType: DialogType.INFO,
+        animType: AnimType.BOTTOMSLIDE,
+        title: 'Dialog Title',
+        desc: showMessage.toString(),
+        btnCancelOnPress: () {},
+        btnOkOnPress: () {},
+      )..show();
+    } else {
+      print(body);
+      print(data);
+      showMessage = body['msg'];
+      AwesomeDialog(
+        context: context,
+        dialogType: DialogType.INFO,
+        animType: AnimType.BOTTOMSLIDE,
+        title: 'Dialog Title',
+        desc: showMessage.toString(),
+        btnCancelOnPress: () {},
+        btnOkOnPress: () {},
+      )..show();
+    }
+  }
+
+
+sendClinicStore({
+    @required context,
+    @required String name,
+    @required String nameBangla,
+    @required String cityId,
+    @required String divisionId,
+    @required String addressLine1,
+    String addressLine2,
+     services,
+     surgeries,
+     testFacilities,
+    @required image,
+    @required String locationLat,
+    @required String locationLng,
+    @required String branchName,
+    @required String receptionPhone,
+  }) async {
+    
+
+    var fullUrl = Uri(
+        scheme: "http",
+        host: "139.59.112.145",
+        path: "/api/registration/clinic/store");
+
+    var data = {
+      'name': name,
+      'nameBangla': nameBangla,
+      'cityId': cityId,
+      'division_id': divisionId,
+      'address_line_1': addressLine1,
+      'address_line_2': addressLine2,
+      'Services': services,
+      'Surgeries': surgeries,
+      'test_facilities': testFacilities,
+      'location_lat': locationLat,
+      'location_lng': locationLng,
+      'branch_name': branchName,
+      'receptionPhone': receptionPhone,
+      'image': image,
+    };
+
+    // FormData formData = new FormData.fromMap(data);
+
+    final response =
+        await http.post(fullUrl, body: json.encode(data), headers: headers);
+
+    String showMessage;
+
+    var body = json.decode(response.body);
+    if (response.statusCode == 200) {
+      showMessage = body['msg'];
+      print(data);
+      print(body);
+      AwesomeDialog(
+        context: context,
+        dialogType: DialogType.INFO,
+        animType: AnimType.BOTTOMSLIDE,
+        title: 'Dialog Title',
+        desc: showMessage.toString(),
+        btnCancelOnPress: () {},
+        btnOkOnPress: () {},
+      )..show();
+    } else {
+      print(body);
+      print(data);
+      showMessage = body['msg'];
+      AwesomeDialog(
+        context: context,
+        dialogType: DialogType.INFO,
+        animType: AnimType.BOTTOMSLIDE,
+        title: 'Dialog Title',
+        desc: showMessage.toString(),
+        btnCancelOnPress: () {},
+        btnOkOnPress: () {},
+      )..show();
+    }
+  }
+
+
+
+
+
+
+
+
 }
